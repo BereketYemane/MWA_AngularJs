5f1eee58c3883219117d31115a88387434cda7cd
BereketYemane <87670119+BereketYemane@users.noreply.github.com>
2021-10-06 23:51:16 -0500

    commit

--- PHP_MEAN/public/angularjs-app/teams/teams-controller.js
+++ PHP_MEAN/public/angularjs-app/teams/teams-controller.js
@@ -2,7 +2,7 @@ angular.module("volleyballTeams").controller("teamsController",teamsController);
 
 function teamsController(teamsFactory){
     const vm = this;
-    vm.country = "VOLLEYBALL TEAMS APP";
+    vm.title = "VOLLEYBALL TEAMS APP";
     teamsFactory.getAllTeams().then(function(response){
         vm.teams = response;
     });
